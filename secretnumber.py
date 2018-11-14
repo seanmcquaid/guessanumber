8a42ae1a4146cd93451eb38f6f60fc56184cf869
seanmcquaid <sean.mcquaid2@verizon.net>
2018-11-13 19:03:53 -0500

    step 2

--- secretnumber.py
+++ secretnumber.py
@@ -1,1 +1,12 @@
 secret_number = 5;
+print "I am thinking of a number between 1 and 10"
+user_guess = 0;
+while (user_guess != secret_number):
+    user_guess = int(raw_input(" What's the number? "))
+    if (user_guess != secret_number):
+        if (user_guess < secret_number):
+            print str(user_guess) + " is too low. Try Again!"
+        else:
+            print str(user_guess) + " is too high. Try Again!"
+    else: 
+        print "You Win!";
