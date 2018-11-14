52e58e9c764fbfbafd3a2e678e53d1e0311110d1
seanmcquaid <sean.mcquaid2@verizon.net>
2018-11-14 11:29:01 -0500

    cleaned up and organized code a bit

--- secretnumber.py
+++ secretnumber.py
@@ -1,28 +1,29 @@
+gameOn = True
 import random
-secret_number = random.randint(1, 10);
-user_guess = 0;
-guesses = 5;
-gameOn = True;
-userName = raw_input("What is your name? ");
+secret_number = random.randint(1, 10)
+guesses = 5
+userName = raw_input("What is your name? ")
 
-print "I am thinking of a number between 1 and 10"
 
 while (gameOn == True):
-    user_guess = int(raw_input(" What's the number? "));
-    guesses -= 1;
+    print "I am thinking of a number between 1 and 10"
+    user_guess = int(raw_input(" What's the number? "))
+    guesses -= 1
+
     if (user_guess != secret_number):
         if (user_guess < secret_number):
-            print str(user_guess) + " is too low. Try Again!";
+            print "%i is too low. Try Again!" % user_guess
         else:
-            print str(user_guess) + " is too high. Try Again!";
+            print "%i is too high. Try Again!" % user_guess
     else: 
-        print "You Win! Congratulations %s" % userName;
+        print "You Win! Congratulations %s" % userName
         break
 
+
     if (guesses == 0 & user_guess != secret_number):
-        print "Sorry %s, you're out of guesses!" % userName;
+        print "Sorry %s, you're out of guesses!" % userName
         break
     elif (guesses == 1):
-        print "You have 1 guess left";
+        print "You have 1 guess left!"
     else:
-        print "You have " + str(guesses) + " guesses left!";
+        print "You have %i guesses left!" % guesses
